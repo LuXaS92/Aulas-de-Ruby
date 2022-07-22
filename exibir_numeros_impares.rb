7740fecee7e1e053ce95a1f3586d4c0f301ce6a4
LuXaS92 <109088944+LuXaS92@users.noreply.github.com>
2022-07-21 22:55:22 -0300

    exibir_numero_impar

--- exibir_numeros_impares.rb
+++ exibir_numeros_impares.rb
@@ -1,3 +1,14 @@
+# exercicio para numero par
+
+limite_minimo = ARGV[0].to_i
+limite_maximo = ARGV[1].to_i
+
+(limite_maximo..limite_minimo).each do |numero|
+    puts numero if numero.even?
+end
+
+# numero impar
+
 limite_minimo = ARGV[0].to_i
 limite_maximo = ARGV[1].to_i
 
