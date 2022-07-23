4d5c3505989ac2af81e3d5954a0e1acb57bc45b6
LuXaS92 <109088944+LuXaS92@users.noreply.github.com>
2022-07-22 21:18:43 -0300

    metodo2

---
+++ metodo2.rb
@@ -0,0 +1,13 @@
+def numeros_impares(min, max)
+    (min..max).each do |numero|
+        puts "O numero #{numero} é impar" if numero.odd?
+    end
+end
+
+def media(elementos)
+    elementos.sum / elementos.size
+end
+
+#numeros_impares(80, 90)
+puts media([9, 1, 2])
+puts media([10, 20])
