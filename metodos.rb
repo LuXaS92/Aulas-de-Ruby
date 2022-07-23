d35708b5110ea093e8bade65e11223b050671c82
LuXaS92 <109088944+LuXaS92@users.noreply.github.com>
2022-07-22 22:14:10 -0300

    final_da_aula

--- metodos.rb
+++ metodos.rb
@@ -1,7 +1,25 @@
-def numeros_impares
-    (0..10).each do |numero|
-        puts "O número #{numero} é impar" if numero.odd?
+def numeros_impares(min = 0, max)
+    (min..max).each do |numero|
+      puts "O número #{numero} é ímpar" if numero.odd?
     end
-end
-
-numeros_impares
+  end
+  
+  def media(elementos)
+    elementos.sum / elementos.size
+  end
+  
+  def rolar_dado(faces)
+    return 'número de faces precisa ser maior que 1' if faces <= 1
+  
+    rand(faces) + 1
+  end
+  
+  numeros_impares(80, 90)
+  puts '---'
+  numeros_impares(12)
+  
+  # puts media([9, 1, 2])
+  # puts media([10, 20])
+  
+  # puts rolar_dado(1)
+  # puts rolar_dado(6)
