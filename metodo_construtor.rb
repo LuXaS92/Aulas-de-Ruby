4eff2821b3131c3c8c52d81b313288ea80bbc250
LuXaS92 <109088944+LuXaS92@users.noreply.github.com>
2022-07-25 13:21:55 -0300

    metodo_construtor

---
+++ metodo_construtor.rb
@@ -0,0 +1,13 @@
+class Pessoa
+    def initialize(nome, idade)
+        @nome  = nome
+        @idade = idade
+    end
+
+    def exibir_informacoes
+        puts "#{@nome} tem #{@idade} anos."
+    end
+end
+
+pessoa = Pessoa.new('Joana', 45)
+pessoa.exibir_informacoes
